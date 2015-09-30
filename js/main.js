9248ab48518bbb0de763614b3a41a70dc0048907
howardchi <howardchi@fougood.com>
2015-09-30 18:00:29 +0800

    fix bug

--- js/main.js
+++ js/main.js
@@ -21,27 +21,6 @@ $(document).ready(function() {
         });
     });
 
-    // Owl Carousel
-    $(document).ready(function() {
-
-        $("#owl-demo").owlCarousel({
-
-            navigation: true, // Show next and prev buttons
-            slideSpeed: 300,
-            paginationSpeed: 400,
-            singleItem: true
-
-            // "singleItem:true" is a shortcut for:
-            // items : 1,
-            // itemsDesktop : false,
-            // itemsDesktopSmall : false,
-            // itemsTablet: false,
-            // itemsMobile : false
-
-        });
-
-    });
-
     // Animate and WOW Animation
     var wow = new WOW(
         {
